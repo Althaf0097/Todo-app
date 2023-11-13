2eb145cd98b4ba78bbeb0e90bc82fc05d62e4c74
Althaf0097 <althafs879@gmail.com>
2023-11-13 20:52:17 +0530

    update App.jsx

--- Todo-App/src/App.jsx
+++ Todo-App/src/App.jsx
@@ -1,34 +1,51 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import AppName from "./components/AppName";
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+  return ( <center 
+  className='todo-container'>
+  <AppName/>
+  <div className = "container text-center">
+  <div className = "row">
+    <div className = "col-6">
+      <input type = "text" placeholder = "Ener Todo Here"/>
+    </div>
+
+    <div className = "col-4">
+      <input type = "date" />
+    </div>
+    <div className =  "col-2">
+    <button type = "button" className = "btn btn-success">Add</button>
+    </div>
+  </div>
+
+  <div className = "row">
+    <div className = "col-6">
+      Buy Milk 
+    </div>
+
+    <div className = "col-4">
+      4/10/2023
+    </div>
+    <div className =  "col-2">
+    <button type = "button" className = "btn btn-danger">Delete</button>
+    </div>
+
+  </div>
+  <div className = "row">
+    <div className = "col-6">
+      go to Colllege
+    </div>
+
+    <div className = "col-4">
+      4/10/2023
+    </div>
+    <div className =  "col-2">
+    <button type = "button" className = "btn btn-success">Add</button>
+    </div>
+  </div>
+</div>
+  </center>
   )
 }
 
